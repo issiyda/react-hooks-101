239ba5a84388023b0fd12a9c11eb182cee8d7187
issiy <allmylifewriter@gmail.com>
2020-10-05 16:56:11 +0900

    hooks学習

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = () => {
-  return (
-    <div>
-      this is a React App
-    </div>
-  );
+
+const App = props => {
+
+    const [name, setName] = useState(props.name);
+    const [price, setPrice] = useState(props.price);
+
+    const reset = () => {
+        setPrice(props.price)
+        setName(props.name)
+    }
+
+    return (
+        <div>
+            <p>現在の{name}は、{price}円です。</p>
+            <button onClick={() => setPrice(price + 1)}>+1</button>
+            <button onClick={() => setPrice(price - 1)}>-1</button>
+            <button onClick={reset}>Reset</button>
+            <input value={name} onChange={e => setName(e.target.value)} />
+        </div>
+    );
+}
+
+App.defaultProps = {
+    name: '',
+    price: 1000
 }
 
 export default App;

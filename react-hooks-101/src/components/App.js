e6f4e2178726047b37d864a28893e06294fb0d36
issiy <allmylifewriter@gmail.com>
2020-10-06 17:27:27 +0900

    stateCreate

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -1,10 +1,27 @@
-import React, {useEffect,useState} from 'react';
+import React, {useState, useReducer} from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const App = props => {
-    const [state, setState] = useState(props);
+import reducer from '../reducers'
 
+const App = props => {
+    const [state, dispatch] = useReducer(reducer, []);
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+    const addEvent = (e) => {
+        e.preventDefault()
+        console.log(title,body);
+        dispatch({
+            type: 'CREATE_EVENT',
+            title,
+            body
+        })
+
+        setTitle('');
+        setBody('');
+
+    }
 
 
     return (
@@ -13,15 +30,15 @@ const App = props => {
             <form>
                 <div className="form-group">
                     <label htmlFor="formEventTitle">タイトル</label>
-                    <input id="formEventTitle" className="form-control" type="text"/>
-                </div>
-                
-                <div className="form-group">
-                    <label htmlFor="formEventBody">ボディー</label>
-                    <textarea className="form-control" name="" id="formEventBody" cols="30" rows="10"></textarea>
+                    <input id="formEventTitle" className="form-control" type="text" value={title} onChange={e => setTitle(e.target.value)}/>
                 </div>
 
-                <button className="btn btn-primary">イベント作成</button>
+                <div className="form-group">
+                    <label htmlFor="formEventBody">ボディー</label>
+                    <textarea className="form-control" name="" id="formEventBody" cols="30" rows="10" value={body} onChange={e => setBody(e.target.value)}></textarea>
+                </div>
+
+                <button className="btn btn-primary" onClick={addEvent}>イベント作成</button>
                 <button className="btn btn-danger">全てのイベントを削除する</button>
                 <button className="btn btn-danger">全ての操作ログをを削除する</button>
             </form>
@@ -29,12 +46,12 @@ const App = props => {
             <h4>イベント一覧</h4>
             <table className="table table-hover">
                 <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>タイトル</th>
-                        <th>ボディー</th>
-                        <th></th>
-                    </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>タイトル</th>
+                    <th>ボディー</th>
+                    <th></th>
+                </tr>
                 </thead>
             </table>
         </div>
--- react-hooks-101/src/reducers/index.js
+++ react-hooks-101/src/reducers/index.js
@@ -1,24 +1,24 @@
 
-action = {
-    type: 'CREATE_EVENT',
-    title: '東京オリンピック',
-    body: '2020年に東京オリンピックが開催されます。'
-};
 
-state = [];
-
-state = [
-    {id:1, title: 'タイトル1', body: 'ボディー1'},
-    {id:2, title: 'タイトル2', body: 'ボディー2'},
-    {id:3, title: 'タイトル3', body: 'ボディー3'},
-]
+// action = {
+//     type: 'CREATE_EVENT',
+//     title: '東京オリンピック',
+//     body: '2020年に東京オリンピックが開催されます。'
+// };
+//
+//
+// state = [
+//     {id:1, title: 'タイトル1', body: 'ボディー1'},
+//     {id:2, title: 'タイトル2', body: 'ボディー2'},
+//     {id:3, title: 'タイトル3', body: 'ボディー3'},
+// ]
 
 const events = (state = [], action) => {
     switch(action.type) {
         case 'CREATE_EVENT':
             const event = { title: action.title, body: action.body}
             const length = state.length;
-            const id = length === 0 ? 1: state[ length - 1].id+1
+            const id = length === 0 ? 1: state[length - 1].id +1;
             return [...state, { id, ...event}]
         case 'DELETE_EVENT':
             return state;

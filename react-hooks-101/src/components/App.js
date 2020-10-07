195f62622efd441df956d3b9792b20bb4e690f14
issiy <allmylifewriter@gmail.com>
2020-10-07 14:19:57 +0900

    context create

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -4,21 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
 import Events from './Events'
+import AppContext from "../contexts/AppContext";
 
-import Event from './Event';
 
 import reducer from '../reducers'
 
+
+
 const App = props => {
     const [state, dispatch] = useReducer(reducer, []);
-    console.log('app')
 
     return (
-        <div className="container-fluid">
-
-            <EventForm state={state} dispatch={dispatch}/>
-            <Events state={state} dispatch={dispatch}/>
-        </div>
+        <AppContext.Provider value={'Hello, Iam a Provider'}>
+            <div className="container-fluid">
+                <EventForm state={state} dispatch={dispatch}/>
+                <Events state={state} dispatch={dispatch}/>
+            </div>
+        </AppContext.Provider>
     );
 }
 
--- react-hooks-101/src/components/Events.js
+++ react-hooks-101/src/components/Events.js
@@ -1,26 +1,28 @@
-import React from "react";
-
-import { DELETE_EVENT } from '../actions'
+import  React , { useContext } from "react";
 
 import Event from "./Event";
+import AppContext from "../contexts/AppContext";
 
-const Events = ({state,dispatch, }) => {
+const Events = ({state, dispatch}) => {
+    const value = useContext(AppContext);
+
     return (
         <>
-            <h4>イベント一覧</h4>
-            <table className="table table-hover">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>タイトル</th>
-                    <th>ボディー</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
-                </tbody>
-            </table>
+                <div>{value}</div>
+                <h4>イベント一覧</h4>
+                <table className="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>タイトル</th>
+                        <th>ボディー</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+                    </tbody>
+                </table>
         </>
     )
 }
